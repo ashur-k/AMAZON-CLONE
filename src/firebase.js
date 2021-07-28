dfb751e54a65124264366ff97a3cadc55fe3d359
ashur-k <ashurkanwal@yahoo.com>
2021-07-28 22:13:55 +0100

    added title section to site

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBJgRL-9_ViF-ek7EZsWAx_4Kku6I5ueyU",
-  authDomain: "clone-2a30b.firebaseapp.com",
-  projectId: "clone-2a30b",
-  storageBucket: "clone-2a30b.appspot.com",
-  messagingSenderId: "98317503062",
-  appId: "1:98317503062:web:5b0ba8d4a40398e12489f7",
-  measurementId: "G-QGCKMQ57MT"
-};
